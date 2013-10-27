28e4ce225c11da914f4f59eb7d7179631af193d7
Matei94 <matei.rm94@gmail.com>
2013-10-27 14:11:21 +0200

    Passes all the tests

--- CGOL/cgol.rb
+++ CGOL/cgol.rb
@@ -1,9 +1,58 @@
+#!/usr/bin/ruby1.9.1
+
 require 'set'
+
+$no_neighbors = 8
+$x_coord = [-1, -1, -1,  0, 0,  1, 1, 1]
+$y_coord = [-1,  0,  1, -1, 1, -1, 0, 1]
 
 class Cell < Struct.new(:x, :y)
 end
 
-def evolve_universe(seed)
-  [].to_set
+def get_neighbors_stats(seed, cell)
+	no_alive_neighbors = 0
+	revived_neighbors = [].to_set
+	
+	$no_neighbors.times do |i|
+		neighbor = Cell.new(cell.x + $x_coord[i], cell.y + $y_coord[i])
+		
+		if seed.include?(neighbor)
+		
+			no_alive_neighbors += 1
+			
+		else
+		
+			no_neighbors_of_neighbor = 0
+			$no_neighbors.times do |j|
+				neighbor_of_neighbor = Cell.new(neighbor.x + $x_coord[j], neighbor.y + $y_coord[j])
+				no_neighbors_of_neighbor += 1 if seed.include?(neighbor_of_neighbor)
+			end
+			
+			revived_neighbors << neighbor if no_neighbors_of_neighbor == 3
+			
+		end
+		
+	end
+	
+	return no_alive_neighbors, revived_neighbors
 end
+
+def evolve_universe(seed)
+	to_add = [].to_set
+	to_remove = [].to_set
+	
+	seed.each do |cell|
+		no_alive_neighbors, revived_neighbors = get_neighbors_stats(seed, cell)
+		to_add += revived_neighbors
+		to_remove << cell if (no_alive_neighbors < 2 || no_alive_neighbors > 3)
+ 	end
+ 	
+ 	seed += to_add
+ 	seed -= to_remove
+ 	
+ 	return seed
+end
+
+seed = [Cell.new(0,0)].to_set
+alive_cells_in_new_generation = evolve_universe(seed)
 
--- CGOL/testsuite.rb
+++ CGOL/testsuite.rb
@@ -1,9 +1,11 @@
+#!/usr/bin/ruby1.9.1 
+
 require 'test/unit'
 require './cgol.rb'
 
 class TestConwaysGameOfLife < Test::Unit::TestCase
 
-=begin
+
   def test_with_an_empty_seed
     # Arrange (a.k.a. define input)
     seed = [].to_set
@@ -19,9 +21,9 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "An empty seed does not evolve into an empty seed as expected"
     )
   end # def test_with_an_empty_seed
-=end
 
-=begin
+
+
   def test_with_a_seed_of_00
     # Arrange (a.k.a. define input)
     seed = [Cell.new(0,0)].to_set
@@ -37,9 +39,9 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of [(0,0)] does not evolve into an empty seed as expected"
     )
   end # def test_with_a_seed_of_00
-=end
 
-=begin
+
+
   def test_with_a_seed_of_00_01
     # Arrange (a.k.a. define input)
     seed =
@@ -58,9 +60,9 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of [(0,0),(0,1)] does not evolve into an empty seed as expected"
     )
   end # def test_with_a_seed_of_00_01
-=end
 
-=begin
+
+
   def test_with_a_seed_of_00_01_10
     # Arrange (a.k.a. define input)
     seed =
@@ -89,9 +91,9 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_00_01_10
-=end
 
-=begin
+
+
   def test_with_a_seed_of_10_21_02_12_22
     # Arrange (a.k.a. define input)
     seed =
@@ -123,9 +125,9 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_10_21_02_12_22
-=end
 
-=begin
+
+
   def test_with_a_seed_of_01_12_13_21_22
     # Arrange (a.k.a. define input)
     seed =
@@ -157,7 +159,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_01_12_13_21_22
-=end
+
 
 end
 
